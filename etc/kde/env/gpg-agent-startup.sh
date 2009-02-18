97ce2629f82638d05a365563d89d3bc32652f197
Rex Dieter <rdieter@math.unl.edu>
2009-02-18 15:24:17 +0000

    Use gpg-agent --write-env-file (http://bugzilla.redhat.com/486025)

--- etc/kde/env/gpg-agent-startup.sh
+++ etc/kde/env/gpg-agent-startup.sh
@@ -12,7 +12,7 @@ if [ -x "${GPG_AGENT}" ] ; then
     if [ ! "x${GPG_PID_NAME}" = "xgpg-agent" ]; then
       rm -f "${GPG_AGENT_INFO_FILE}" 2>&1 >/dev/null
     else
-       GPG_SOCKET=`cat "${GPG_AGENT_INFO_FILE}" | cut -f1 -d:`
+       GPG_SOCKET=`cat "${GPG_AGENT_INFO_FILE}" | cut -f1 -d: | cut -f2 -d=`
        if ! test -S "${GPG_SOCKET}" -a -O "${GPG_SOCKET}" ; then
          rm -f "${GPG_AGENT_INFO_FILE}" 2>&1 >/dev/null
        fi
@@ -21,11 +21,10 @@ if [ -x "${GPG_AGENT}" ] ; then
   fi
 
   if [ -f "${GPG_AGENT_INFO_FILE}" ]; then
-    export GPG_AGENT_INFO=$(cat "${GPG_AGENT_INFO_FILE}")
-    export GPG_TTY=$(tty)
+    eval "$(cat \"${GPG_AGENT_INFO_FILE}\")"
+    eval "$(cut -d= -f 1 < \"${GPG_AGENT_INFO_FILE}\" | xargs echo export)"
   else
-    eval "$(${GPG_AGENT} -s --daemon ${GPG_OPTIONS})"
-    echo ${GPG_AGENT_INFO} > "${HOME}/.gpg-agent-info"
+    eval "$(${GPG_AGENT} -s --daemon --write-env-file \"${GPG_AGENT_INFO_FILE}\" ${GPG_OPTIONS})"
   fi
 
 fi
