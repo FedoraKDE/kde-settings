528af6d75c53c266b71836aead036b0587160630
Rex Dieter <rdieter@math.unl.edu>
2011-12-05 15:06:01 +0000

    import first iteration of build-it script, an attempt to automate updating (and building) all kde modules in fedpkg

---
+++ build-it.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+pkg=$1
+kde=4.7.90
+
+set -x
+
+if [ -d "${pkg}/" ]; then
+pushd "${pkg}"
+fedpkg switch-branch master
+fedpkg pull
+else
+fedpkg clone "${pkg}"
+pushd "${pkg}"
+fi
+
+#fedpkg switch-branch f16 && fedpkg pull
+
+#git merge master && fedpkg push
+
+## update sources
+# maybe grep sources instead?  -- rex
+if [ ! -f ${pkg}-${kde}.tar.bz2 -a -f  "../kde4/${pkg}-${kde}.tar.bz2" ]; then
+cp -alf "../kde4/${pkg}-${kde}.tar.bz2" .
+fedpkg new-sources ${pkg}-${kde}.tar.bz2
+fi
+
+## update spec
+sed -i \
+  -e "s|Version:.*|Version: ${kde}|" \
+  -e 's|Release:.*|Release: 0%{?dist}|' \
+  ${pkg}.spec
+
+rpmdev-bumpspec --comment="${kde}" ${pkg}.spec
+
+fedpkg commit --clog -p &&
+fedpkg build --background --nowait 
+#--target f16-kde
+
+popd
+
+set -x
+
+echo ${pkg} >> builds.txt
+sleep 2
