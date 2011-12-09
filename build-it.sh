9353416d55aa5d8d43bd5a6f7b49fd354ff583e4
Rex Dieter <rdieter@math.unl.edu>
2011-12-09 13:56:43 +0000

    support merging, and a host of other goodies

--- build-it.sh
+++ build-it.sh
@@ -2,22 +2,45 @@
 
 pkg=$1
 src=${2:-${pkg}}
+
+# which git branch to operate on
+branch=master
+
+## if defined, merge from this branch rather than editing anything
+#merge=master
+
+## koji options
+#koji_opts=--background
+#koji_opts=--target f16-kde
+
+# log builds done to this file
+build_log=build-log.txt
+
 kde=4.7.90
 
+# copious debug output for now
 set -x
 
 if [ -d "${pkg}/" ]; then
 pushd "${pkg}"
-fedpkg switch-branch master
+fedpkg switch-branch ${branch} && \
 fedpkg pull
 else
 fedpkg clone "${pkg}"
 pushd "${pkg}"
 fi
 
-#fedpkg switch-branch f16 && fedpkg pull
 
-#git merge master && fedpkg push
+
+if [ ! -z "${merge}" ]; then
+
+fedpkg switch-branch ${merge} && fedpkg pull 
+
+fedpkg switch-branch ${branch} && \
+git merge ${merge} && \
+fedpkg push
+
+else
 
 ## update sources
 # maybe grep sources instead?  -- rex
@@ -34,13 +57,15 @@ sed -i \
 
 rpmdev-bumpspec --comment="${kde}" ${pkg}.spec
 
-fedpkg commit --clog -p &&
-fedpkg build --background --nowait 
-#--target f16-kde
+fi
+
+fedpkg commit --clog -p && \
+fedpkg build --nowait ${koji_opts}
 
 popd
 
-set -x
+echo ${pkg} >> ${build_log} 
 
-echo ${pkg} >> builds.txt
-sleep 2
+# stupid requirement @ rex's site, whose IT assumes you're a hacker 
+# for doing > 25 outgoing ssh connects in 5 minutes
+sleep 15
