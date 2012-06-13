d7fa24e894c18461c67096cc1152f069e31c4fd0
Rex Dieter <rdieter@math.unl.edu>
2012-06-13 14:30:29 +0000

    use full path to lspci, and capture stderr

--- etc/profile.d/qt-graphicssystem.sh
+++ etc/profile.d/qt-graphicssystem.sh
@@ -4,7 +4,7 @@ if [ -z "${QT_GRAPHICSSYSTEM_CHECKED}" -a -z "${QT_GRAPHICSSYSTEM}" ] ; then
   export QT_GRAPHICSSYSTEM_CHECKED
 
   # workarond cirrus/qt bug, http://bugzilla.redhat.com/810161
-  if ( lspci | grep -qi "VGA compatible controller: Cirrus Logic GD 5446" ) ; then
+  if ( /usr/sbin/lspci 2>/dev/null | grep -qi "VGA compatible controller: Cirrus Logic GD 5446" ) ; then
     QT_GRAPHICSSYSTEM=native
     export QT_GRAPHICSSYSTEM
   fi
