be70568513bd8dd52ebd2f9d1701f9959f92c762
Rex Dieter <rdieter@math.unl.edu>
2012-09-05 18:09:18 +0000

    update for 4.9.1

--- build-it.sh
+++ build-it.sh
@@ -7,7 +7,7 @@ src=${2:-${pkg}}
 branch=master
 
 ## if defined, merge from this branch rather than editing anything
-#merge=f17
+#merge=master
 
 ## koji options
 #koji_opts="--background"
@@ -18,7 +18,7 @@ fi
 # log builds done to this file
 build_log=build-log.txt
 
-kde=4.9.0
+kde=4.9.1
 
 # true if fedpkg prep should be executed before pushing
 use_prep="true"
