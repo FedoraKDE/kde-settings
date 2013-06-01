47b1030ae10060df61b33f2fe2648f8f9d68714d
Rex Dieter <rdieter@math.unl.edu>
2013-06-01 13:59:30 +0000

    update for 4.10.4, and do a 'git reset' to be on the safe side. I (rex) am bad about sometimes leaving testing/unpushed stuff around, which messes things up.

--- build-it.sh
+++ build-it.sh
@@ -18,7 +18,7 @@ fi
 # log builds done to this file
 build_log=build-log.txt
 
-kde=4.10.1
+kde=4.10.4
 
 # true if fedpkg prep should be executed before pushing
 use_prep="true"
@@ -28,6 +28,7 @@ set -x
 
 if [ -d "${pkg}/" ]; then
 pushd "${pkg}"
+git reset --hard HEAD && \
 fedpkg switch-branch ${branch} && \
 fedpkg pull
 else
