5df5fbb0ddc0060a9822b8a8cba290e58043560f
Rex Dieter <rdieter@math.unl.edu>
2015-09-22 01:01:58 +0000

    XDG_CONFIG_DIRS support

--- etc/xdg/plasma-workspace/env/env.sh
+++ etc/xdg/plasma-workspace/env/env.sh
@@ -1,5 +1,11 @@
 ## from http://standards.freedesktop.org/menu-spec/latest/
 
+# XDG_CONFIG_DIRS
+if [ -z "${XDG_CONFIG_DIRS}" ] ; then
+   XDG_CONFIG_DIRS=/etc/xdg:/usr/share/kde-settings/kde-profile/default/xdg
+   export XDG_CONFIG_DIRS
+fi
+
 # XDG_DATA_DIRS
 if [ -z "${XDG_DATA_DIRS}" ] ; then
    XDG_DATA_DIRS=/usr/share/kde-settings/kde-profile/default/share:/usr/local/share:/usr/share
