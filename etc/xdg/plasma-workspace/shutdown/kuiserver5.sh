02acf26e9ce0cfe024d426d295de135fa23cb6a3
Rex Dieter <rdieter@math.unl.edu>
2015-10-08 18:42:14 +0000

    add bug reference

--- etc/xdg/plasma-workspace/shutdown/kuiserver5.sh
+++ etc/xdg/plasma-workspace/shutdown/kuiserver5.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+## Workaround to kuiserver5 processes lingering after logout, see
+## https://bugs.kde.org/show_bug.cgi?id=348123
+
 qdbus-qt5 org.kde.kuiserver >& /dev/null && \
 qdbus-qt5 org.kde.kuiserver /MainApplication org.qtproject.Qt.QCoreApplication.quit
 
